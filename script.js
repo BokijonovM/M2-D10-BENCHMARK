e7376d2ce543c6b60fe70eff9ada62e25b03e0ec
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-19 11:40:29 +0100

    i have done some parts

---
+++ script.js
@@ -0,0 +1,12 @@
+const hideAppear = function () {
+    let iconId = document.getElementById("search-icon-nav")
+    let searchInput = document.getElementById("nav-form-control")
+    iconId.style.display = "none"
+    searchInput.style.display = "block"
+}
+const hideAppear1 = function () {
+    let iconId = document.getElementById("search-icon-nav")
+    let searchInput = document.getElementById("nav-form-control")
+    iconId.style.display = "block"
+    searchInput.style.display = "none"
+}
